dddc5563228f15eb6cf7b3bac932aecee7439b9a
SAKKHAR MAJUMDER <106582628+Sakkhor-omrito@users.noreply.github.com>
2022-12-20 10:58:14 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,22 @@ $('.counter').counterUp({
     delay: 10,
     time: 1000
 });
+
+$(function(){
+    let menuTop=$('.CustomNav').offset().top;
+
+    $(window).on('scroll',function(){
+
+
+        let scrollToTop=$(window).scrollTop();
+        
+        if(scrollToTop>menuTop){
+          $('.CustomNav').addClass("menufix");
+        }else{
+          $('.CustomNav').removeClass("menufix");
+        }
+        
+        
+        });
+
+});
